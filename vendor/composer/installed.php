6173a02a17edf4ef3f4f4b4ff1e55015bc65f30e
Michael Meneses <michael.douglas.meneses.2@gmail.com>
2024-07-29 17:33:29 -0300

    UPDATE twilio/sdk 8.x

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'livehelperchat/twilio',
+        'pretty_version' => '1.0',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'livehelperchat/twilio' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '8.2.2',
+            'version' => '8.2.2.0',
+            'reference' => '10dbd6ecbc6ac1e0c61ab7d70c44e0794f8d3a1f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
